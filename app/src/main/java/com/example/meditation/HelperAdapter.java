15e6807d8015a0b8a1ffd3f5caac65335359b88f
Darkslimes <frozen_throne96@mail.ru>
2021-11-16 17:03:41 +0500

    pized

---
+++ app/src/main/java/com/example/meditation/HelperAdapter.java
@@ -0,0 +1,11 @@
+package com.example.meditation;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HelperAdapter extends RecyclerView.Adapter<HelperAdapter.MyViewClass> {
+
+    ArrayList<String> id;
+    ArrayList<String>
+}
--- app/src/main/java/com/example/meditation/home.java
+++ app/src/main/java/com/example/meditation/home.java
@@ -1,14 +1,20 @@
 package com.example.meditation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.HorizontalScrollView;
 
 public class home extends AppCompatActivity {
+    HorizontalScrollView horizontalScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        horizontalScrollView = findViewById(R.id.horizontalScrollView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        horizontalScrollView.setLayoutManager(linearLayoutManager);
     }
 }
